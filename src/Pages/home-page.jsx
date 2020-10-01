74a60027362153adc67b7c2aa105f89f200e6033
Karishma Saini <ksaini7595@gmail.com>
2020-09-30 23:02:45 -0400

    adding a homepage, about page

---
+++ src/Pages/home-page.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Jumbotron, Container} from 'reactstrap';
+import { Link } from 'react-router-dom';
+
+
+import '../Styles/home.scss';
+
+const HomePage = () => (
+    <div>
+    <div className='homepage'>
+        <div className='options' >
+        <Link className='option' to='/about'>
+    ABOUT
+    </Link>
+    <Link className='option' to='/shop'>
+   PROJECTS
+    </Link>
+    <span className='nav-logo'>  GS </span>
+    <Link className='option' to='/shop'>
+  CONTACT
+    </Link>
+    <Link className='option' to='/shop'>
+LINKEDIN
+    </Link>
+        </div>
+    </div>
+    <hr className='hr-bar'/>
+
+
+    <h1 className='title-home'><em>GEORGE SAMUELS II</em></h1>
+    <div>
+      <Jumbotron className='home-container'fluid>
+        <Container fluid>
+          <h1 className="display-3">IMAGE</h1>
+         
+        </Container>
+        <h4 className='home-project'>View Projects</h4>
+      </Jumbotron>
+    </div>
+    </div>
+   
+  
+)
+
+export default HomePage;
